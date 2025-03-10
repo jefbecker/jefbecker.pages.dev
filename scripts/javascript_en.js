27a35863a1571ac6b861de442cff70a72d3b3574
jefbecker <gpg@jefbecker.com>
2025-03-09 21:09:39 -0300

    Update files

--- scripts/javascript_en.js
+++ scripts/javascript_en.js
@@ -1,6 +1,6 @@
 // Headless - How to Change Your Neocities Thumbnail from ARandomSite: https://arandomsite.neocities.org/writing/#custom-neocities-thumbnail //
 
-// const urlParams = new URLSearchParams(window.location.search); if (navigator.userAgent.includes('Screenjesus')) { window.location.replace("/thumbnail/"); } //
+const urlParams = new URLSearchParams(window.location.search); if (navigator.userAgent.includes('Screenjesus')) { window.location.replace("/thumbnail/"); }
 
 // Hitcount_en - Native Neocities Hit-Counter & Last Updated From Dann: https://dannarchy.com/tut/tut_002 //
 
